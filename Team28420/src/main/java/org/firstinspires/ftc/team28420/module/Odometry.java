591b8183884eb9089a3a9e367c75b61b20276357
wydentis <sapetp54@gmail.com>
2026-04-22 10:20:31 +0300

    fix(teleop): fix teleop controls and add start pos for odometry

--- Team28420/src/main/java/org/firstinspires/ftc/team28420/module/Odometry.java
+++ Team28420/src/main/java/org/firstinspires/ftc/team28420/module/Odometry.java
@@ -1,18 +1,24 @@
 package org.firstinspires.ftc.team28420.module;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.team28420.types.Position;
 
 public class Odometry {
 
+    @Config
     public static class OdometryConfig {
         public static double OFFSET_X = 24.871;
         public static double OFFSET_Y = -43.235;
+        public static double START_POS_X = 20;
+        public static double START_POS_Y = 20;
+        public static double START_HEADING = 0;
     }
 
     private final GoBildaPinpointDriver pinpoint;
@@ -29,6 +35,9 @@ public class Odometry {
                 GoBildaPinpointDriver.EncoderDirection.FORWARD
         );
         pinpoint.resetPosAndIMU();
+        // TODO adjust robot start position
+        pinpoint.setPosition(new Pose2D(DistanceUnit.CM, OdometryConfig.START_POS_X,
+                OdometryConfig.START_POS_Y, AngleUnit.RADIANS, OdometryConfig.START_HEADING));
     }
 
     public void update() {
